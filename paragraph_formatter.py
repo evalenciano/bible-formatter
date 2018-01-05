d450c70725bae1658cbf57479092de49b08a17db
Elijah Valenciano <elijah_valenciano@comcast.com>
2018-01-04 23:59:00 -0500

    Make sure script actually runs

---
+++ paragraph_formatter.py
@@ -0,0 +1,15 @@
+import sys
+import json
+
+def main():
+    book = sys.argv[1]
+    chapter = sys.argv[2]
+    starting_verse = sys.argv[3]
+    end_verse = sys.argv[4]
+
+    print('You are looking for ' + book + ' ' +
+    chapter + ':' + starting_verse + '-' + end_verse)
+
+    data = json.load(open('ESV.json'))
+
+main()
