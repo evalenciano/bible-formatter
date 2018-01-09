81a8b97f458ef689ab09e902707de30b9f53462d
Elijah Valenciano <elijah_valenciano@comcast.com>
2018-01-08 22:47:43 -0500

    Print out requested verses onto the console

--- paragraph_formatter.py
+++ paragraph_formatter.py
@@ -4,12 +4,18 @@ import json
 def main():
     book = sys.argv[1]
     chapter = sys.argv[2]
-    starting_verse = sys.argv[3]
+    initial_verse = sys.argv[3]
     end_verse = sys.argv[4]
 
     print('You are looking for ' + book + ' ' +
-    chapter + ':' + starting_verse + '-' + end_verse)
+    chapter + ':' + initial_verse + '-' + end_verse)
 
     data = json.load(open('NIV.json'))
+
+    verse_count = int(initial_verse)
+    while (verse_count <= int(end_verse)):
+        string_verse = data[book][chapter][str(verse_count)]
+        print(string_verse)
+        verse_count = verse_count + 1
 
 main()
